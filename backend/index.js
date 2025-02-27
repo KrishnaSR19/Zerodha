584d7111be24657e692bbaa4a0198043fa026c45
Krishna <coderks36@gmail.com>
2025-02-27 23:26:15 +0530

    backend model

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,19 @@
+require('dotenv').config();
+ 
+
 const express =require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3002;
+const url = process.env.MONGO_URL;
 
 const app = express();
+
+
+
 app.listen(3002,()=>{
     console.log("App started!");
+    mongoose.connect(url);
+    console.log("DB connected!");
+
 });
