977c3745146ed081f50a6a85adb3450471994a17
Krishna <coderks36@gmail.com>
2025-03-01 15:28:23 +0530

    Setting up for Auth and Authentication

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
-const {HoldingsModel } = require("./model/HoldingsModel");
-const {PositionsModel} = require("./model/PositionsModel");
- 
+require("dotenv").config();
+const { HoldingsModel } = require("./model/HoldingsModel");
+const { PositionsModel } = require("./model/PositionsModel");
+const { OrdersModel } = require("./model/OrdersModel");
 
-const express =require("express");
+const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const PORT = process.env.PORT || 3002;
@@ -16,25 +16,29 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-
-
-app.get("/allHoldings",async(req,res)=>{
+app.get("/allHoldings", async (req, res) => {
   let allHoldings = await HoldingsModel.find({});
   res.json(allHoldings);
-
-})
-
-app.get("/allPositions",async(req,res)=>{
-  let allPositions = await PositionsModel.find({});
-  res.json(allPositions);
-
-})
-
-app.listen(3002,()=>{
-    console.log("App started!");
-    mongoose.connect(url);
-    console.log("DB connected!");
-
 });
 
+app.get("/allPositions", async (req, res) => {
+  let allPositions = await PositionsModel.find({});
+  res.json(allPositions);
+});
 
+app.post("/newOrder", async (req, res) => {
+  let newOrder = new OrdersModel({
+    name: req.body.name,
+    qty: req.body.qty,
+    price: req.body.price,
+    mode: req.body.mode,
+  });
+  newOrder.save();
+  res.send("Order saved!");
+});
+
+app.listen(3002, () => {
+  console.log("App started!");
+  mongoose.connect(url);
+  console.log("DB connected!");
+});
